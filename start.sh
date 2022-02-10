90d09c38b65c4c6ed0ef121344cea4a85d2c4c27
Titicojoao <jovmarques65@gmail.com>
2022-02-10 18:21:09 -0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#! bin/bash
+VERDE= ' \033[0;32m '
+enquanto  : 
+Faz
+echo  " ${GREEN} INICIANDO O  BOT
+ESTE ARQUIVO É PROGRAMADO PARA O BOT NÃO CAIR
+APÓS ALGUMA QUEDA DE CONEXÃO "
+    nó index.js
+    dormir 1
+
+feito
